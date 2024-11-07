36a5c2f89b8a2fd13a97d330a8e6edb0cd18886c
parkjaehak <atom8426@gmail.com>
2024-11-07 12:33:02 +0900

    Init: image set 확인

--- src/main/java/org/codingtext/helloworld/HealthCheckController.java
+++ src/main/java/org/codingtext/helloworld/HealthCheckController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
     @GetMapping
     public String healthcheck() {
-        return "fucking world";
+        return "rollling  world";
     }
 
 }
