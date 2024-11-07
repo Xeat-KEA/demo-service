d9870a89073b5b519380df0f65d32b989f63368f
parkjaehak <atom8426@gmail.com>
2024-11-07 11:58:39 +0900

    Init: 변경

--- src/main/java/org/codingtext/helloworld/HealthCheckController.java
+++ src/main/java/org/codingtext/helloworld/HealthCheckController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
     @GetMapping
     public String healthcheck() {
-        return "Hello world";
+        return "fucking world";
     }
 
 }
